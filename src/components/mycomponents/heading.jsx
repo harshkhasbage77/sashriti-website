d7e0ec0fb1d53220839abf17bf64b9004bf5ad90
harshkhasbage77 <harshkhasbage77@gmail.com>
2025-04-13 16:08:16 +0530

    Start: Magic UI + Next.js + Tailwind CSS setup done

---
+++ src/components/mycomponents/heading.jsx
@@ -0,0 +1,11 @@
+import { AuroraText } from "@/components/magicui/aurora-text";
+
+export function AuroraTextDemo() {
+  return (
+    <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
+      Reclaiming <AuroraText>Value.</AuroraText> 
+      <br />
+      Rebuilding <AuroraText>Tomorrow.</AuroraText>
+    </h1>
+  );
+}
