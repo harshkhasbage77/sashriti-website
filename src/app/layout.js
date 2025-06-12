faef279be17792b5a9e2f3dc5b6d33e39f1ecb60
harshkhasbage77 <harshkhasbage77@gmail.com>
2025-06-13 03:51:30 +0530

    Screenshot added in preview image

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,28 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Sashriti - Know The Real Value",
   description: "Protector of wealth",
+  openGraph: {
+    title: "Sashriti - Know The Real Value",
+    description: "Protector of wealth",
+    url: "https://sashriti-website.vercel.app", // or your production domain
+    siteName: "Sashriti",
+    images: [
+      {
+        url: "/preview.png", // MUST be in the public folder
+        width: 1200,
+        height: 630,
+        alt: "Sashriti Website Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sashriti - Know The Real Value",
+    description: "Protector of wealth",
+    images: ["/preview.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
